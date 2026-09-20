355d6419b82b714170af541882250740d1bd7697
Syed Haris Hussain Shah <syedharishussainshah17@gmail.com>
2026-09-20 16:29:36 -0700

    Update port configuration

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Backend is working!");
